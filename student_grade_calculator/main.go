47f37d557bdfd8887b3e325f84a9715c6c611f54
Your Name <you@example.com>
2024-08-05 14:28:53 +0300

    update student grade calculater

--- student_grade_calculator/main.go
+++ student_grade_calculator/main.go
@@ -4,38 +4,9 @@ import (
 	"fmt"
 )
 
-func main() {
-	stu_grade_calc := StudentGradeCalculator{}
-	fmt.Println("Welcome to my non-AI-powered grade calculator")
-	fmt.Print("Your Name Please: ")
-	fmt.Scan(&stu_grade_calc.studentName)
-	fmt.Print("Number of subjects you took: ")
-	fmt.Scan(&stu_grade_calc.numberOfSubjects)
-	for i := 0; i < stu_grade_calc.numberOfSubjects; i++ {
-		subject := Subject{}
-		fmt.Printf("Subject %v\n", i + 1)
-
-		fmt.Print("\tEnter Name: ")
-		fmt.Scan(&subject.subject_name)
-		
-		fmt.Print("\tEnter Grade (0 - 100): ")
-		fmt.Scan(&subject.grade)
-		for !valid(subject.grade) {
-			fmt.Print("\tPlease Enter a Valid Grade between 0 and 100: ")
-			fmt.Scan(&subject.grade)
-		}
-		stu_grade_calc.subjects = append(stu_grade_calc.subjects, subject)
-	}
-	fmt.Printf("Your average grade is %2f.", stu_grade_calc.average_grade())
-}
-
-func valid(grade float64) bool {
-	return grade >= 0 && grade <= 100
-}
-
 type Subject struct {
-	subject_name string
-	grade        float64
+	name  string
+	grade float64
 }
 
 type StudentGradeCalculator struct {
@@ -44,12 +15,55 @@ type StudentGradeCalculator struct {
 	subjects         []Subject
 }
 
-func (SGC StudentGradeCalculator) average_grade() float64 {
+func main() {
+	stuGradeCalc := StudentGradeCalculator{}
+
+	fmt.Println(" --- Welcome to my non-AI-powered average grade calculator --- ")
+
+	fmt.Print("Your Name Please: ")
+	fmt.Scan(&stuGradeCalc.studentName)
+
+	fmt.Print("Number of subjects you took: ")
+	fmt.Scan(&stuGradeCalc.numberOfSubjects)
+
+	for stuGradeCalc.numberOfSubjects <= 0 {
+		fmt.Print("Please Enter a Valid Number of Subjects: ")
+		fmt.Scan(&stuGradeCalc.numberOfSubjects)
+	}
+
+	for i := 0; i < stuGradeCalc.numberOfSubjects; i++ {
+		subject := Subject{}
+
+		fmt.Printf("Subject %v:\n", i+1)
+
+		fmt.Print("\tEnter Name: ")
+		fmt.Scan(&subject.name)
+
+		fmt.Print("\tEnter Grade (0 - 100): ")
+		fmt.Scan(&subject.grade)
+
+		for !isValidGrade(subject.grade) {
+			fmt.Print("\tPlease Enter a Valid Grade between 0 and 100: ")
+			fmt.Scan(&subject.grade)
+		}
+
+		stuGradeCalc.subjects = append(stuGradeCalc.subjects, subject)
+	}
+
+	fmt.Printf("\nYour average grade is %.2f.", stuGradeCalc.calculateAverageGrade())
+}
+
+func isValidGrade(grade float64) bool {
+	return grade >= 0 && grade <= 100
+}
+
+func (sgc StudentGradeCalculator) calculateAverageGrade() float64 {
 	total := 0.0
-	for _, subject := range SGC.subjects {
+
+	for _, subject := range sgc.subjects {
 		total += subject.grade
 	}
 
-	average_grade := total / float64(SGC.numberOfSubjects)
-	return average_grade
+	averageGrade := total / float64(sgc.numberOfSubjects)
+	return averageGrade
 }
